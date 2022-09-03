ee81ba14dc4e9d9537ab9bc0c4d0b72535e87b40
FatihG34 <fthgnydn34@gmail.com>
2022-09-03 08:06:35 +0300

    forms done

--- pizza/forms.py
+++ pizza/forms.py
@@ -1,13 +1,13 @@
 from django import forms
-
 from pizza.models import Pizza
 
 
 class PizzaForm(forms.ModelForm):
-    model = Pizza
-    fields= ['topping1', 'topping2', 'size']
-    labels = {'topping1':'Topping_1', 'topping2':'Topping_2', 'size': 'Size'}
+    class Meta:
+        model = Pizza
+        fields= ['topping1', 'topping2', 'size']
+        labels = {'topping1':'Topping_1', 'topping2':'Topping_2', 'size': 'Size'}
 
 
 class MultiOrderingForm(forms.Form):
-    number=forms.IntegerField('Number', required=False)
+    number=forms.IntegerField(required=False)
--- pizza/urls.py
+++ pizza/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from pizza.views import home, ordering_pizza
+
 urlpatterns = [
     path("", home, name='home'),
+    path("order/", ordering_pizza, name='order'),
 ]
